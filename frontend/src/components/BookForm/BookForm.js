419cf52519d230000451570da31627295a68b466
Oleksandr Bakai <saniabakay@gmail.com>
2026-01-22 18:48:45 +0100

    Add book source

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -15,7 +15,7 @@ const BookForm = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
 
-    dispatch(addBook(createBookWithId(randomBook)));
+    dispatch(addBook(createBookWithId(randomBook, 'random')));
   };
 
   const handleSubmit = (e) => {
@@ -25,7 +25,7 @@ const BookForm = () => {
       //dispatch action
 
       //send to store
-      dispatch(addBook(createBookWithId({title, author})));
+      dispatch(addBook(createBookWithId({title, author}, 'manual')));
       setTitle('');
       setAuthor('');
     }
@@ -35,7 +35,7 @@ const BookForm = () => {
     try {
       const res = await axios.get('http://localhost:4000/random-book');
       if (res.data && res.data.title && res.data.author) {
-        dispatch(addBook(createBookWithId(res.data)));
+        dispatch(addBook(createBookWithId(res.data,'API')));
       }
     } catch (error) {
       console.log('Error fetching random book', error);
