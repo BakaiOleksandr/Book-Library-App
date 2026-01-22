10f9d615878b5f93db4d99665d01e7331a06f435
Oleksandr Bakai <saniabakay@gmail.com>
2026-01-22 23:37:28 +0100

    bookSlice>eztraReducers Options

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -52,6 +52,17 @@ const bookSlice = createSlice({
       });
     },
   },
+
+  //OPTION 1
+  // extraReducers: {
+  //   [fetchBook.fulfilled]: (state, action) => {
+  //     if (action.payload.title && action.payload.author) {
+  //       return [...state, createBookWithId(action.payload, 'API')];
+  //     }
+  //   },
+  // },
+
+  //OPTION 2
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
       if (action.payload.title && action.payload.author) {
